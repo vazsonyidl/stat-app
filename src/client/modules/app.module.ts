328b4406fe9e9b0f186b872cb0806b8b7123060d
danielvazsonyi <daniel.vazsonyi@balabit.com>
2020-11-11 20:24:38 +0100

    config(app): create proxy configuration for backend

--- src/client/modules/app.module.ts
+++ src/client/modules/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from '../components/app/app.component';
 import {rootConfig} from "../configs/routing/routing";
-import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
