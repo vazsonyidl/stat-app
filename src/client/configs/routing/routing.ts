89f05fdd2446a91808950ba7e2aad53d7e1f2573
danielvazsonyi <daniel.vazsonyi@balabit.com>
2020-11-28 15:10:10 +0100

    feat(app-routing): add routing - default path and fallbacks

--- src/client/configs/routing/routing.ts
+++ src/client/configs/routing/routing.ts
@@ -1,6 +1,13 @@
 import {RouterModule, Routes} from "@angular/router";
 import {ModuleWithProviders} from '@angular/core';
+import {MapComponent} from "../../pages/map/map.component";
+import {NotFoundPageComponent} from "../../pages/not-found/not-found.page";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'map'},
+  {path: 'map', component: MapComponent},
+  {path: '404', component: NotFoundPageComponent},
+  {path: '**', redirectTo: '404'}
+];
 
 export const rootConfig: ModuleWithProviders<any> = RouterModule.forRoot(routes);
