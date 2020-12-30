521d779b82395e73cc8a6fc892a369aa9ca15cfd
vazsonyidl <danielvazsonyi@gmail.com>
2020-12-30 13:00:46 +0100

    style(app): remove unused imports and style file

--- src/client/pages/not-found/not-found.module.ts
+++ src/client/pages/not-found/not-found.module.ts
@@ -1,10 +1,7 @@
 import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
-
 import {NotFoundPageComponent} from './not-found.page';
 
 @NgModule({
-  imports: [RouterModule,],
   declarations: [NotFoundPageComponent],
   exports: [NotFoundPageComponent],
 })
