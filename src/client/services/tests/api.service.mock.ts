295ee4a8b7e66f751fb94002b382afd1256d4434
vazsonyidl <danielvazsonyi@gmail.com>
2021-02-24 17:48:27 +0100

    test(api-service): update api-service unit tests

---
+++ src/client/services/tests/api.service.mock.ts
@@ -0,0 +1,36 @@
+import {SearchSchemaVariable} from 'components/search/search.interface';
+import {HttpErrorResponse} from '@angular/common/http';
+
+export const dummySchema: Array<SearchSchemaVariable> = [
+  {
+    code: 'Maakond',
+    text: 'County',
+    values: ['00', '37'],
+    valueTexts: ['Whole country', 'Harju county'],
+  }, {
+    code: 'Aasta',
+    text: 'Year',
+    values: ['2004', '2005'],
+    valueTexts: ['2004', '2005'],
+  }
+];
+
+export const dummySearchResult = {
+  columns: [
+    {code: 'Maakond', text: 'County', type: 'd'},
+    {code: 'Aasta', text: 'Year', type: 't'},
+    {code: 'HT042: PREPRIMARY INSTITUTIONS', text: 'HT042: PREPRIMARY INSTITUTIONS', type: 'c'}
+  ],
+  comments: [],
+  data: [
+    {
+      key: ['37', '2006'],
+      values: ['205']
+    }
+  ],
+  metadata: [{
+    updated: '2013-07-04T05:00:00Z',
+    label: 'HT042: PREPRIMARY INSTITUTIONS by County and Year',
+    source: 'Statistics Estonia'
+  }]
+};
